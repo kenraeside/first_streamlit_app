f84238faf8a5fede90d30a0292a8fca3107a29c5
Ken Raeside <ken.raeside@gmail.com>
2024-01-18 13:28:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -18,6 +18,6 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
+streamlit.dataframe(my_fruit_list)
 
 
