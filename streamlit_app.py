9bd7b142aa48f0ccb8819b0dc2167da4f64e0670
Ken Raeside <ken.raeside@gmail.com>
2024-01-26 15:02:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.title('A New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
